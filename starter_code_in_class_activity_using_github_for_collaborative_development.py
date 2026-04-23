5cd85b9fda864c00e14b8f49b351dbc25a292292
ethanw920 <mcdubster@gmail.com>
2026-04-23 09:49:04 -0500

    removed text

--- starter_code_in_class_activity_using_github_for_collaborative_development.py
+++ starter_code_in_class_activity_using_github_for_collaborative_development.py
@@ -1,14 +1,6 @@
 import math
 
-#### TASK 1 - ENTER YOUR TEAM NAME AND NUMBER
-
-
 team_name = "Quinn-Ethan-TyS310"
-
-#### TASK 2 - Clone the empty gitHub repo in your local computer (Member #1)
-
-#### TASK 3 - Add this code to your gitHub repo - follow the best practices of add --> commit --> pull --> push
-
 
 def calculator():
   print("Calculator by team =  " + team_name)
@@ -74,27 +66,3 @@ def exponent(number1, number2): #Quin
   return math.pow(number1, number2)
 
 calculator()
-
-
-#### TASK 4 (Member #2)- pull the gitHub repo in your local computer  and implement the SUBTRACTION function()
-
-#### TASK 5 (Member #2)- Add the implemented the SUBTRACTION function() to your gitHub repo in a new branch called feature/subtract
-
-#### TASK 6 (Member #3) - pull the gitHub repo in your local computer and implement the Multiplication function()
-
-#### TASK 7 (Member #3) - Add the implemented the Multiplication function() to your gitHub repo in a new branch called feature/mult
-
-
-#### TASK 8 (Member #4)- pull the gitHub repo in your local computer  and implement the Division function()
-
-#### TASK 9 (Member #4)- Add the implemented the Division function() to your gitHub repo in a new branch called feature/div
-
-
-#### TASK 8 (Member #1)- pull the gitHub repo in your local computer  and implement the IntegerDivision function()
-
-#### TASK 9 (Member #1)- Add the implemented the IntegerDivision function() to your gitHub repo in the existing branch feature/div
-
-
-#### TASK 10 (Member #2)- pull the gitHub repo in your local computer  and implement the SQRT function()
-
-#### TASK 11 (Member #2)- Add the implemented the SQRT function() to your gitHub repo in the main branch
